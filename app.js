b929e88d488b9acfd0019c24d44f9901cb58716e
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-11-07 10:58:23 -0300

    Limit clicking on wrong tile functionality

--- app.js
+++ app.js
@@ -35,9 +35,54 @@ const boardModule = (function() {
     }
 })()
 
-const playerModule = (function() {
+const gameModule = (function() {
     const spaces = document.querySelectorAll('.board-space')
-    spaces.forEach(space => space.addEventListener('click', () => {
-        boardSpaces[space.dataset.index].mark = 'X'
-    }))
+    let turnOver = false;
+    
+    function interact(playermark){
+        spaces.forEach(space => space.addEventListener('click', () => {
+            addMark(playermark, space)
+        }))
+    }
+    
+    function addMark(playermark, space) {
+        if((space.textContent === '') && turnOver === false) {
+            boardSpaces[space.dataset.index].mark = playermark
+            space.textContent = playermark
+            turnOver = true
+        } 
+    }
+
+
+    return {
+        interact,
+    }
 })()
+
+const playerModule = (function(){
+    function playerCreator(name, mark, computer) {
+        const playerName = name
+        const playerMark = mark
+        const playerScore = 0
+        
+        const getPlayerScore = () => playerScore
+        const updatePlayerScore = () => playerScore++
+        const playerAction = () => gameModule.interact(playerMark)
+
+        return {
+            playerName,
+            playerMark,
+            updatePlayerScore,
+            getPlayerScore,
+            playerAction
+        }
+    }
+    // const fede = playerCreator('Fede', 'X')
+    // fede.playerAction()
+
+    return {
+        playerCreator,
+    }
+})()
+
+// alternating turn system with recursion and % of 2 with variable that adds 1 after turnOver is set to true, unless finish condition is met (finish condition is first if)
