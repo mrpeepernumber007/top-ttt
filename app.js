36521e624c5714b5f498b5c0e7e953a8d8f62b3d
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-11-07 13:58:36 -0300

    Failed attempts

--- app.js
+++ app.js
@@ -35,22 +35,24 @@ const boardModule = (function() {
     }
 })()
 
-const gameModule = (function() {
+const playModule = (function() {
     const spaces = document.querySelectorAll('.board-space')
-    let turnOver = false;
-    
+    //turnOver has to come in from turnModule, use turnOver as an argument in playModule
+    let turnOver = true
+
     function interact(playermark){
         spaces.forEach(space => space.addEventListener('click', () => {
-            addMark(playermark, space)
+            turnOver = !turnOver;
+            addMark(playermark, space, turnOver)
         }))
     }
     
-    function addMark(playermark, space) {
-        if((space.textContent === '') && turnOver === false) {
+    function addMark(playermark, space, turnOver) {
+        if((space.textContent === '') && !turnOver) {
             boardSpaces[space.dataset.index].mark = playermark
             space.textContent = playermark
             turnOver = true
-        } 
+        }
     }
 
 
@@ -67,22 +69,52 @@ const playerModule = (function(){
         
         const getPlayerScore = () => playerScore
         const updatePlayerScore = () => playerScore++
-        const playerAction = () => gameModule.interact(playerMark)
+        // const playerAction = () => playModule.interact(playerMark)
 
         return {
             playerName,
             playerMark,
             updatePlayerScore,
             getPlayerScore,
-            playerAction
+            // playerAction
         }
     }
-    // const fede = playerCreator('Fede', 'X')
-    // fede.playerAction()
+
 
     return {
         playerCreator,
     }
 })()
 
+const fede = playerModule.playerCreator('Fede', 'X')
+const pepe = playerModule.playerCreator('Pepe', 'O')
+
+const nextTurn = document.querySelector('.next-turn-btn')
+let turnCount = 0
+
+const turnModule = function() {
+
+    if(turnCount % 2 === 0) {
+        fede.playerAction()
+        turnCount++
+    } else if (turnCount % 2 !== 0) {
+        pepe.playerAction()
+        turnCount++
+    }
+}
+
+nextTurn.addEventListener('click', turnModule)
+
+
+// const turnModule = (function(){
+//     const fede = playerModule.playerCreator('Fede', 'X')
+//     let turnOver = false
+//     playModule(turnOver)
+//     return {
+//         fede,
+//         turnOver
+//     }
+// })()
+
 // alternating turn system with recursion and % of 2 with variable that adds 1 after turnOver is set to true, unless finish condition is met (finish condition is first if)
+// turn system with event listener, alternating x and o, and checks for victory conditions
