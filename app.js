92204e8eb919d8b0aa376b89a99b1b973cf13bbe
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-11-09 20:41:49 -0300

    Finish easy ai implementation

--- app.js
+++ app.js
@@ -62,30 +62,34 @@ const playerModule = (function(){
 })()
 
 const player1 = playerModule.playerCreator('Fede', 'X', false)
-const player2 = playerModule.playerCreator('Pepe', 'O', false)
+const player2 = playerModule.playerCreator('Pepe', 'O', true)
 
 const playModule = (function() {
     const spaces = document.querySelectorAll('.board-space')
     let p1Choices = []
     let p2Choices = []
+    let availableSpots = JSON.parse(JSON.stringify(boardSpaces))
     
     function interact(){
         let turnNum = 0;
-        spaces.forEach(space => space.addEventListener('click', () => {
-            if (space.textContent === '') {
-                if ((turnNum % 2 === 0) && (player2.playerAI === false)){
-                    addMark(space, player1, p1Choices)
-                } else if ((turnNum % 2 !== 0) && (player2.playerAI === false)) {
-                    addMark(space, player2, p2Choices)
-                } else if ((turnNum % 2 === 0) && (player2.playerAI === true)) {
-                    //missing ai parameter
-                    //when player1 clicks, player2AI makes a play
-                    addMark(space, player1, p1Choices)
-                    addMark(space, player2, p2Choices, true)
+        spaces.forEach(space => {
+            space.addEventListener('click', () => {
+                if (space.textContent === '') {
+                    if ((turnNum % 2 === 0) && (player2.playerAI === false)){
+                        addMark(space, player1, p1Choices)
+                    } else if ((turnNum % 2 !== 0) && (player2.playerAI === false)) {
+                        addMark(space, player2, p2Choices)
+                    } else if ((turnNum % 2 === 0) && (player2.playerAI === true)) {
+                        //missing ai parameter
+                        //when player1 clicks, player2AI makes a play
+                        //for impossible ai make it choose from winCon
+                        addMark(space, player1, p1Choices)
+                        addMark(space, player2, p2Choices, true)
+                        turnNum++
+                    }
+                    turnNum++
                 }
-                turnNum++
-            }
-        }))
+        })})
     }
 
     function playerVictory(choices, player) {
@@ -93,7 +97,7 @@ const playModule = (function() {
         if(check) {
             choices = []
             player.updatePlayerScore()
-            return console.log(player.getPlayerScore());
+            return console.log(`${player.name}'s score is ` + player.getPlayerScore());
         }
     }
     
@@ -105,6 +109,17 @@ const playModule = (function() {
             choices.push(boardSpaces[space.getAttribute('data-index')].name)
         } else if (ai) {
             //ai implementation here
+            function recursiveAi(){
+                const chosen = Math.floor(Math.random()*spaces.length)
+                if(spaces[chosen].textContent === '') {
+                    boardSpaces[chosen].mark = player.playerMark
+                    spaces[chosen].textContent = player.playerMark
+                    choices.push(boardSpaces[chosen].name)
+                } else {
+                    recursiveAi()
+                }
+            }
+            recursiveAi()
         }
             
         playerVictory(choices, player)
@@ -165,3 +180,30 @@ const endModule = (function (){
 
 //to end the game, revise boardModule to enable resetting the board
 //in playerVictory reset boardSpaces value to boardSpaces = []
+//reset availableSpots too
+
+// function interact(){
+//         let turnNum = 0;
+//         for (let i = 0; i < spaces.length; i++) {
+//             if (spaces[i].textContent === '') {
+//                 availableSpots.splice(i, 1)
+//                 console.log(availableSpots);
+//             }
+//             spaces[i].addEventListener('click', () =>{
+//                 if (spaces[i].textContent === '') {
+//                     if ((turnNum % 2 === 0) && (player2.playerAI === false)){
+//                         addMark(spaces[i], player1, p1Choices)
+//                     } else if ((turnNum % 2 !== 0) && (player2.playerAI === false)) {
+//                         addMark(spaces[i], player2, p2Choices)
+//                     } else if ((turnNum % 2 === 0) && (player2.playerAI === true)) {
+//                         //missing ai parameter
+//                         //when player1 clicks, player2AI makes a play
+//                         //for impossible ai make it choose from winCon
+//                         addMark(spaces[i], player1, p1Choices)
+//                         addMark(spaces[i], player2, p2Choices, true)
+//                     }
+//                     turnNum++
+//                 }
+//             })
+//         }
+//     }
